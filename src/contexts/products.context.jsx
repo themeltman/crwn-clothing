f2659ff3a7bfc4810ef59f56cce72b871ead5ebe
David Meltzer <dmeltzer@zixcorp.com>
2022-08-26 13:37:31 -0500

    Write shop date to database in Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,6 @@
-import {createContext, useState} from "react";
-import PRODUCTS from '../shop-data.json'
+import {createContext, useEffect, useState} from "react";
+import {addCollectionAndDocuments} from "../utils/firebase/firebase.utils";
+import SHOP_DATA from "../shop-data";
 
 
 export const ProductsContext = createContext({
@@ -7,7 +8,10 @@ export const ProductsContext = createContext({
     })
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS)
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        addCollectionAndDocuments('categories', SHOP_DATA)
+    }, [])
     const value = { products }
     return (
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDRqxdWlgyxYuOI9_2POHY_bNgTOGw7IZw",
@@ -21,6 +21,18 @@ export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 export const db = getFirestore()
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey)
+    const batch = writeBatch(db)
+
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase())
+        batch.set(docRef, object)
+    })
+    await batch.commit()
+    console.log("Batch done")
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     const userDocRef = doc(db, 'users', userAuth.uid)
