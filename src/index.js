892f3cf28aef1ec27f2f53549e9c760f3e9a967f
David Meltzer <dmeltzer@zixcorp.com>
2022-09-16 17:32:26 -0500

    persist redux store

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import App from './App';
 
 import './index.scss';
 import {Provider} from "react-redux";
-import {store} from "./store/store";
+import {persistor, store} from "./store/store";
+import {PersistGate} from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
+          <PersistGate persistor={persistor}>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+          </PersistGate>
       </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import {compose, createStore, applyMiddleware} from 'redux'
-import {logger} from "redux-logger";
+// import {logger} from "redux-logger";
 import {rootReducer} from "./root-reducer";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
 const loggerMiddleware = (store) => (next) => (action) => {
     if (!action.type) {
@@ -15,7 +17,17 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
     console.log('nextState: ', store.getState())
 }
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 const middleWares = [loggerMiddleware]
 const composedEnhancers = compose(applyMiddleware(...middleWares))
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)
